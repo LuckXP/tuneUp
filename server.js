cff95dfd5b8935f554e8ab58ca2c8dba71b83a2d
LuckXP <js.sindler@gmail.com>
2018-01-14 18:07:14 -0700

    added dotenv and env file

--- server.js
+++ server.js
@@ -2,9 +2,19 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const router = express.Router()
+require('dotenv').config()
 const mongoose = require('mongoose')
 const port = process.env.PORT || 8080
 const playlists = require('./routes/playlists')
+const methodOverride = require('method-override')
+const session = require('express-session')
+const passport = require('passport')
+const appKey = process.env.SPOTIFY_CLIENT_ID;
+const appSecret = process.env.SPOTIFY_CLIENT_SECRET;
+
+console.log('heeeeeeeeeeeeerrrrrrrrrrrrreeeeeeeeeeeee!#!#!#!#!#!##!#!',appKey,appSecret);
+
+
 
 mongoose.connect('mongodb://localhost/tuneup')
 
@@ -16,7 +26,7 @@ router.use('/playlists', playlists)
 app.set('view engine', 'ejs');
 
 app.get('*', function(req, res){
-  res.sendFile('./index.html'));
+  res.sendFile('./index.html');
 })
 
 app.listen(port)
